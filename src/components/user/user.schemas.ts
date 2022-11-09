133e1ee3db49759eceb43ae32827a7d26e2f41c7
OMAR-777 <omar.omar5745@gmail.com>
2022-11-09 16:48:28 +0300

    first template commit

---
+++ src/components/user/user.schemas.ts
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+import { IValidationSchema } from '../../utils/joi.interfaces';
+
+export const getUserValidation: IValidationSchema = {
+  params: Joi.object({
+    id: Joi
+      .number()
+      .required(),
+  }).required(),
+};
+
+export const createUserValidation: IValidationSchema = {
+  body: Joi.object({
+    firstname: Joi
+      .string()
+      .min(2)
+      .required(),
+    lastname: Joi
+      .string()
+      .min(2)
+      .required(),
+    email: Joi
+      .string()
+      .email()
+      .required(),
+    password: Joi
+      .string()
+      .min(8)
+      .required(),
+  }).required(),
+};
---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { IUserSerialized } from '../components/user/user.interfaces';
+
+export class JWT {
+  static sign(user: IUserSerialized){
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+    process.env.JWT_KEY!,
+    );
+    return token;
+  }
+
+  static verify(token: string){
+    const result = jwt.verify(token, process.env.JWT_KEY!);
+
+    return result;
+  }
+}
