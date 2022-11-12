45fe14ae6f3797149200e40e03ee27d256842976
OMAR-777 <omar.omar5745@gmail.com>
2022-11-12 22:17:15 +0300

    create product table

---
+++ migrations/sqls/20221112135639-product-table-up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "product" (
+  "id" SERIAL,
+  "name" VARCHAR(255) NOT NULL,
+  "price" VARCHAR(255) NOT NULL,
+  "category" VARCHAR(255),
+  "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY("id")
+);
