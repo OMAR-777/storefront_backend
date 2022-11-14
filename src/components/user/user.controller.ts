1f16d58eac939b01c0a4fe1e23f9301708b1c649
OMAR-777 <omar.omar5745@gmail.com>
2022-11-14 10:45:12 +0300

    formatting

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -8,36 +8,32 @@ import CustomResponse from '../../utils/custom-response';
 import { JWT } from '../../utils/jwt';
 
 class UserController {
-
-  async getProfile(req: Request, res: Response){
+  async getProfile(req: Request, res: Response) {
     CustomResponse.send(res, { profile: req.user });
   }
 
-  async getUser(req: Request, res: Response){
+  async getUser(req: Request, res: Response) {
     const user = await User.findOneById(+req.params.id);
-    if(!user){
+    if (!user) {
       throw new NotFoundError('User Not Found!');
     }
     CustomResponse.send(res, { user });
   }
 
-  async getUsers(req: Request, res: Response){
+  async getUsers(req: Request, res: Response) {
     const users = await User.findAll();
     CustomResponse.send(res, { users });
   }
 
-  async login(req: Request, res: Response){
-    const {
-      email,
-      password,
-    } = req.body;
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
     const user = await User.findOneByEmail(email);
-    if(!user){
+    if (!user) {
       return CustomResponse.sendWithError(res, 'Invalid Credentials!', 404);
     }
 
     const isMatch = await Password.compare(user.password, password);
-    if(isMatch){
+    if (isMatch) {
       const token = JWT.sign(user);
 
       const userSerialization = user as IUserSerialized;
@@ -45,49 +41,49 @@ class UserController {
 
       const result = { user: userSerialization, token };
       CustomResponse.send(res, result, `Welcome Back, ${user.firstname}`);
-    }else{
+    } else {
       return CustomResponse.sendWithError(res, 'Invalid Credentials!', 400);
     }
   }
 
-  async signUp(req: Request, res: Response){
-    const {
-      firstname,
-      lastname,
-      email,
-      password,
-    } = req.body;
+  async signUp(req: Request, res: Response) {
+    const { firstname, lastname, email, password } = req.body;
 
     const existingUser = await User.findOneByEmail(email);
-    if(existingUser){
-      throw new BadRequestError('There\'s a user with this email already!');
+    if (existingUser) {
+      throw new BadRequestError("There's a user with this email already!");
     }
     const hashedPassword = await Password.toHash(password);
 
-    const dataObject: ICreateUser = { firstname, lastname, email, password: hashedPassword };
+    const dataObject: ICreateUser = {
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    };
 
     const user = await User.create(dataObject);
-    if(user){
+    if (user) {
       // Creating a JWT token for this user, and returning it back in the response
       // so that it can be used in the Authentication process
       const token = JWT.sign(user);
       const result = { user, token };
 
       return CustomResponse.send(res, result, 'Created Successfully', 201);
-    }else{
+    } else {
       throw new Error();
     }
   }
 
-  async createMany(req: Request, res: Response){
+  async createMany(req: Request, res: Response) {
     const users = req.body.Users as ICreateUser[];
     let insertedUsers: IUserSerialized[] = [];
-    let errorUsers: [reason: string,user: ICreateUser][] = [];
+    let errorUsers: [reason: string, user: ICreateUser][] = [];
 
-    for(let i=0; i<users.length; i++){
+    for (let i = 0; i < users.length; i++) {
       var userFound = await User.findOneByEmail(users[i].email);
-      if(userFound){
-        errorUsers.push(['There\'s a user with this email already!',users[i]]);
+      if (userFound) {
+        errorUsers.push(["There's a user with this email already!", users[i]]);
         continue;
       }
 
@@ -95,29 +91,42 @@ class UserController {
       users[i].password = hashedPassword;
 
       const newUser = await User.create(users[i]);
-      if(newUser){
+      if (newUser) {
         insertedUsers.push(newUser);
-      }else{
-        errorUsers.push(['Could not insert this user',users[i]]);
+      } else {
+        errorUsers.push(['Could not insert this user', users[i]]);
       }
-
     }
 
     const result = {
-      insertedCount:insertedUsers.length,
+      insertedCount: insertedUsers.length,
       errorCount: errorUsers.length,
       insertedUsers,
       errorUsers,
     };
 
-    if(insertedUsers.length){
-      if(errorUsers.length == 0){
-        return CustomResponse.send(res, insertedUsers, 'All Users Created Successfully', 201);
-      }else{
-        return CustomResponse.send(res, result, 'Some Users Created Successfully', 201);
+    if (insertedUsers.length) {
+      if (errorUsers.length == 0) {
+        return CustomResponse.send(
+          res,
+          insertedUsers,
+          'All Users Created Successfully',
+          201,
+        );
+      } else {
+        return CustomResponse.send(
+          res,
+          result,
+          'Some Users Created Successfully',
+          201,
+        );
       }
-    }else{
-      return CustomResponse.sendWithErrorAndData(res, result, 'No User is Created Susccessfully');
+    } else {
+      return CustomResponse.sendWithErrorAndData(
+        res,
+        result,
+        'No User is Created Susccessfully',
+      );
     }
   }
 }
