2b573e5fd699a970d126418e8e92fe4e219a2745
OMAR-777 <omar.omar5745@gmail.com>
2022-11-15 12:32:20 +0300

    add id for order interfaces

--- src/components/order/order.interfaces.ts
+++ src/components/order/order.interfaces.ts
@@ -3,25 +3,27 @@ export enum OrderStatus {
   Completed = "Completed",
 }
 
+export interface ICreateOrder {
+  user_id: number;
+  status: OrderStatus
+}
+
 export interface IAddOrderProduct {
   order_id: number;
   product_id: number;
   quantity: number;
 }
 
-export interface IOrderProduct {
-  product_id: number;
-  quantity: number;
-}
-
-export interface ICreateOrder {
-  user_id: number;
-  status: OrderStatus
-}
-
 export interface IOrder {
+  id: number,
   user_id: number;
   status: OrderStatus;
   orderProducts: IOrderProduct[];
   created_at: string;
+}
+
+export interface IOrderProduct {
+  id: number,
+  product_id: number;
+  quantity: number;
 }
