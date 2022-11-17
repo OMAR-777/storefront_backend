8273dae0dea47cb5510ae703961e67a3b98cd0bd
OMAR-777 <omar.omar5745@gmail.com>
2022-11-17 12:07:51 +0300

    handle invalid signatures

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { IUser, IUserSerialized } from '../components/user/user.interfaces';
 import User from '../components/user/user.model';
+import { BadRequestError } from '../errors/bad-request-error';
+import Logger from './logger';
 
 interface UserPayload {
   id: number;
@@ -24,20 +26,20 @@ export const currentUser = async (
   if (!req.headers.authorization) {
     return next();
   }
+  try {
+    const bearerToken = req.headers.authorization; // "Bearer blablablablablabla"
+    const token = bearerToken.split(' ')[1];
+    const payload = jwt.verify(token, process.env.JWT_KEY!) as UserPayload;
 
-  const bearerToken = req.headers.authorization;  // "Bearer blablablablablabla"
-  const token = bearerToken.split(' ')[1];
-  const payload = jwt.verify(
-    token,
-    process.env.JWT_KEY!,
-  ) as UserPayload;
+    const user = await User.findOneById(payload.id);
+    if (!user) {
+      return next();
+    }
 
-  const user = await User.findOneById(payload.id);
-  if(!user){
-    return next();
+    req.user = user;
+  } catch (e) {
+    Logger.error('JWT invalid signature');
   }
-
-  req.user = user;
 
   next();
 };
